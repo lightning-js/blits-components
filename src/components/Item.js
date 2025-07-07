96d8c0e40587a473d844975ebd3d5da16c4c7d50
Robert-Jan <robertjankeizer@gmail.com>
2025-07-07 15:05:42 +0200

    Fixed a bug in the list where multiple items got a focus

--- src/components/Item.js
+++ src/components/Item.js
@@ -22,13 +22,13 @@ export default Blits.Component('Item', {
     <Element
       w="$width"
       h="$height"
-      :color="$hasFocus ? '#fff' : '#888'"
+      :color="$isFocused ? '#fff' : '#888'"
       :effects="[{type: 'radius', props: {radius: $radius}}]"
     >
       <Text content="$item.label" color="#121212" size="$height/2" mount="0.5" x="$width/2" y="$height/2" />
     </Element>
   `,
-  props: ['item', 'width', 'height'],
+  props: ['item', 'width', 'height', 'isFocused'],
   state() {
     return {
       radius: 6,
